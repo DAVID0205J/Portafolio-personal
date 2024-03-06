7bc84f762b48514a4807467cded2065d0990887c
DAVID0205J <jdaceituno@scl.edu.gt>
2024-03-06 13:41:18 -0600

    header

---
+++ header.js
@@ -0,0 +1,12 @@
+let header=`
+    <div class="logo">RM</div>
+    <nav class="header_grid">
+        <div>opcion1</div>
+        <div>opcion2</div>
+        <div>opcion3</div>
+        <div>opcion4</div>
+        <div>opcion5</div>
+    </nav>
+    <div class="btn_header"></div>
+`
+export {header}
