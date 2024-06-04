1ebe8735fb235612b4c4851f13d798c60032c527
David <jose.aceituno.balconi@gmail.com>
2024-06-03 21:23:50 -0600

    responsive de la pagina (telefono)

--- js/cuadroLateral.js
+++ js/cuadroLateral.js
@@ -1,7 +1,12 @@
 let cuadroLateral = `
     <div class="info">
-        <div class="log">JD</div>
-        <div class="nombre">DAVID0205J</div>\
+        <div class="nombre">DAVID0205J</div>    
+        <div class="formateo1">
+        <h4>Formateos</h4>
+        <div class="formateo">
+        <p> Lenovo Thinkpad T460  x2</p>
+        </div>
+        </div>
 
     </div>
 `;
