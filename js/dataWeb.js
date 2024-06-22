debb622cf9bd8f40eacca8f97482c4b42ab74773
David <jose.aceituno.balconi@gmail.com>
2024-06-21 22:09:26 -0600

    se agrego el portafolio

--- js/dataWeb.js
+++ js/dataWeb.js
@@ -1,5 +1,12 @@
 let webs = [
     {
+        nombre: "Tienda Virtual Grupal",
+        img :"./img/tienda.png",
+        repositorio: "https://github.com/anleu04/Grupo1_Tienda_2024/",
+        githubpage: "https://anleu04.github.io/Grupo1_Tienda_2024/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    },
+    {
         nombre: "Juego de Memoria",
         img :"./img/cartas .png",
         repositorio: "https://github.com/DAVID0205J/Juego_memoria/",
@@ -19,21 +26,51 @@ let webs = [
         repositorio: "https://github.com/DAVID0205J/Cambio-de-color-",
         githubpage: "https://david0205j.github.io/Cambio-de-color-/",
         descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
-    }
-    ,    {
-        nombre: "Caluculadora",
+        },
+        {
+            nombre: "TodoList",
+            img :"./img/todolist.png",
+            repositorio: "https://github.com/DAVID0205J/todolist",
+            githubpage: "https://david0205j.github.io/todolist/",
+            descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+        },
+        {
+            nombre: "Clone",
+            img :"./img/clone.png",
+            repositorio: "https://github.com/DAVID0205J/clone_de_burgerking",
+            githubpage: "https://david0205j.github.io/clone_de_burgerKing/",
+            descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+        },
+       {
+            nombre: "Caluculadora",
         img :"./img/calculadora.png",
         repositorio: "https://github.com/DAVID0205J/calculadora",
         githubpage: "https://david0205j.github.io/calculadora/",
         descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
     },
     {
-        nombre: "TodoList",
-        img :"./img/todolist.png",
-        repositorio: "https://github.com/DAVID0205J/todolist",
-        githubpage: "https://david0205j.github.io/todolist/",
+        nombre: "IMC",
+        img :"./img/IMC.png",
+        repositorio: "https://github.com/DAVID0205J/IMC",
+        githubpage: "https://david0205j.github.io/IMC/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    },
+    {
+        nombre: "Generador",
+        img :"./img/generador.png",
+        repositorio: "https://github.com/DAVID0205J/generador",
+        githubpage: "https://david0205j.github.io/generador/",
+        descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
+    },
+    {
+        nombre: "Visualizador",
+        img :"./img/visualizador.png",
+        repositorio: "https://github.com/DAVID0205J/visualizador",
+        githubpage: "https://david0205j.github.io/visualizador/",
         descripcion: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero velit facere assumenda corrupti at accusantium obcaecati iusto, tempora expedita aspernatur reiciendis non ratione enim voluptatibus, consequuntur ex ullam voluptas rem? "
     }
+    
+
 
 ];
 
